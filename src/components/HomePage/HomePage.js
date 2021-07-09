9790bb43568f248121180c647153c055d4268459
aishcann <aish.cann@gmail.com>
2021-07-09 15:11:30 -0400

    heroku backend url added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,39 +1,37 @@
-import './homepage.scss'
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom' 
+import './homepage.scss';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
+	const [events, setEvents] = useState([]);
 
-		const [events, setEvents] = useState([])
+	useEffect(() => {
+		fetch('https://in-your-city-api.herokuapp.com/events/')
+			.then((res) => res.json())
+			.then((res) => {
+				setEvents(res);
+			})
+			.catch(console.error);
+	});
 
-		useEffect(() => {
-			fetch('http://localhost:8000/events/')
-				.then((res) => res.json())
-				.then((res) => {
-					setEvents(res)
-				})
-				.catch(console.error)
-				;
-		});
-
-    return (
-			<div className='homePage'>
-				<section className='eventList'>
-					{events.map((event) => {
-						return (
-							<div className='eventInfo'>
-								<h3>{event.name}</h3>
-								<h4>{event.date}</h4>
-								<h4>{event.time}</h4>
-								<h4>{event.location}</h4>
-								<a href={event.link}>{event.link}</a>
-								<p>{event.description}</p>
-							</div>
-						);
-					})}
-				</section>
-			</div>
-		);
+	return (
+		<div className='homePage'>
+			<section className='eventList'>
+				{events.map((event) => {
+					return (
+						<div className='eventInfo'>
+							<h3>{event.name}</h3>
+							<h4>{event.date}</h4>
+							<h4>{event.time}</h4>
+							<h4>{event.location}</h4>
+							<a href={event.link}>{event.link}</a>
+							<p>{event.description}</p>
+						</div>
+					);
+				})}
+			</section>
+		</div>
+	);
 };
 
 export default HomePage;
--- src/components/ManageEvents/ManageEvents.js
+++ src/components/ManageEvents/ManageEvents.js
@@ -1,18 +1,17 @@
-import './manageevents.scss'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import './manageevents.scss';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ManageEvents = () => {
+	const url = 'https://in-your-city-api.herokuapp.com/events/';
 
-	const url = 'http://localhost:8000/events/';
+	const [newEvent, setNewEvent] = useState(true);
 
-	const [newEvent, setNewEvent] = useState(true)
+	const [updateEvent, setUpdateEvent] = useState(false);
 
-	const [updateEvent, setUpdateEvent] = useState(false)
+	const [deleteEvent, setDeleteEvent] = useState(false);
 
-	const [deleteEvent, setDeleteEvent] = useState(false)
-
-	const [id, setId] = useState('')
+	const [id, setId] = useState('');
 
 	const initialState = {
 		name: '',
@@ -23,7 +22,7 @@ const ManageEvents = () => {
 		description: '',
 	};
 
-	const [formState, setFormState] = useState(initialState)
+	const [formState, setFormState] = useState(initialState);
 
 	const handleChange = (e) => {
 		setFormState({ ...formState, [e.target.id]: e.target.value });
@@ -32,149 +31,146 @@ const ManageEvents = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		setNewEvent(!newEvent)
+		setNewEvent(!newEvent);
 		fetch(`${url}${id}`, {
-			method: (updateEvent ? 'PUT' : 'POST'),
+			method: updateEvent ? 'PUT' : 'POST',
 			headers: {
-				'Content-Type': 'application/json'
+				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify(formState),
 		})
-			.then(res => res.json())
-			.then(event => {
-				setId(event.id)
-				setUpdateEvent(false)
-				setNewEvent(false)
-				setFormState(event)
-			})
+			.then((res) => res.json())
+			.then((event) => {
+				setId(event.id);
+				setUpdateEvent(false);
+				setNewEvent(false);
+				setFormState(event);
+			});
 	};
 
 	const handleClick = (e) => {
 		fetch(url + id, {
-			method: 'DELETE'
+			method: 'DELETE',
 		})
-		.then(console.log('removed'))
-		.catch(console.error)
-	}
+			.then(console.log('removed'))
+			.catch(console.error);
+	};
 
-
-
-    return (
-			<div className='createEvents'>
-				{newEvent ? (
-					<form className='createEvent'>
-						<h3>Create Your Event</h3>
-						<h4>Event Name</h4>
-						<input
-							type='text'
-							className='text'
-							id='name'
-							onChange={handleChange}
-							value={formState.name}
-						/>
-						<h4 className='date-time'>
-							Date
-							<br />
-							YYYY-MM-DD Format
-						</h4>
-						<input
-							type='text'
-							className='date'
-							id='date'
-							onChange={handleChange}
-							value={formState.date}
-						/>
-						<h4 className='date-time'>
-							Time
-							<br />
-							24HR 00:00:00 Format
-						</h4>
-						<input
-							type='text'
-							className='time'
-							id='time'
-							onChange={handleChange}
-							value={formState.time}
-						/>
-						<h4>Location</h4>
-						<input
-							type='text'
-							className='text'
-							id='location'
-							onChange={handleChange}
-							value={formState.location}
-						/>
-						<h4>Link</h4>
-						<input
-							type='url'
-							className='text'
-							id='link'
-							onChange={handleChange}
-							value={formState.link}
-						/>
-						<h4>Description</h4>
-						<input
-							type='textarea'
-							className='text'
-							id='description'
-							onChange={handleChange}
-							value={formState.description}
-						/>
-						<button type='submit' onClick={(e) => handleSubmit(e)}>
-							{updateEvent ? 'Update Event' : 'Create Event'}
-						</button>
-					</form>
-				) : null}
-				{!newEvent && !deleteEvent ? (
-					<section className='eventCreated'>
-						<h3>Congrats! Here are your event details!</h3>
-						<p>
-							<b>Name: </b>
-							{formState.name}
-						</p>
-						<p>
-							<b>Date: </b>
-							{formState.date}
-						</p>
-						<p>
-							<b>Time: </b>
-							{formState.time}
-						</p>
-						<p>
-							<b>Location: </b>
-							{formState.location}
-						</p>
-						<p>
-							<b>Link: </b>
-							{formState.link}
-						</p>
-						<p>
-							<b>Description:</b>:
-							{formState.description}{' '}
-						</p>
-						<button
-							onClick={() => {
-								setNewEvent(true);
-								setUpdateEvent(true);
-							}}>
-							I want to edit my event
-						</button>
-						<button
-							onClick={() => {
-								handleClick();
-								setDeleteEvent(true);
-							}}>
-							I want to delete my event
-						</button>
-					</section>
-				) : null}
-				{deleteEvent ? (
-					<section className='eventDeleted'>
-						<h3> Your event was deleted.</h3>
-					</section>
-				) : null}
-			</div>
-		);
+	return (
+		<div className='createEvents'>
+			{newEvent ? (
+				<form className='createEvent'>
+					<h3>Create Your Event</h3>
+					<h4>Event Name</h4>
+					<input
+						type='text'
+						className='text'
+						id='name'
+						onChange={handleChange}
+						value={formState.name}
+					/>
+					<h4 className='date-time'>
+						Date
+						<br />
+						YYYY-MM-DD Format
+					</h4>
+					<input
+						type='text'
+						className='date'
+						id='date'
+						onChange={handleChange}
+						value={formState.date}
+					/>
+					<h4 className='date-time'>
+						Time
+						<br />
+						24HR 00:00:00 Format
+					</h4>
+					<input
+						type='text'
+						className='time'
+						id='time'
+						onChange={handleChange}
+						value={formState.time}
+					/>
+					<h4>Location</h4>
+					<input
+						type='text'
+						className='text'
+						id='location'
+						onChange={handleChange}
+						value={formState.location}
+					/>
+					<h4>Link</h4>
+					<input
+						type='url'
+						className='text'
+						id='link'
+						onChange={handleChange}
+						value={formState.link}
+					/>
+					<h4>Description</h4>
+					<input
+						type='textarea'
+						className='text'
+						id='description'
+						onChange={handleChange}
+						value={formState.description}
+					/>
+					<button type='submit' onClick={(e) => handleSubmit(e)}>
+						{updateEvent ? 'Update Event' : 'Create Event'}
+					</button>
+				</form>
+			) : null}
+			{!newEvent && !deleteEvent ? (
+				<section className='eventCreated'>
+					<h3>Congrats! Here are your event details!</h3>
+					<p>
+						<b>Name: </b>
+						{formState.name}
+					</p>
+					<p>
+						<b>Date: </b>
+						{formState.date}
+					</p>
+					<p>
+						<b>Time: </b>
+						{formState.time}
+					</p>
+					<p>
+						<b>Location: </b>
+						{formState.location}
+					</p>
+					<p>
+						<b>Link: </b>
+						{formState.link}
+					</p>
+					<p>
+						<b>Description:</b>:{formState.description}{' '}
+					</p>
+					<button
+						onClick={() => {
+							setNewEvent(true);
+							setUpdateEvent(true);
+						}}>
+						I want to edit my event
+					</button>
+					<button
+						onClick={() => {
+							handleClick();
+							setDeleteEvent(true);
+						}}>
+						I want to delete my event
+					</button>
+				</section>
+			) : null}
+			{deleteEvent ? (
+				<section className='eventDeleted'>
+					<h3> Your event was deleted.</h3>
+				</section>
+			) : null}
+		</div>
+	);
 };
 
 export default ManageEvents;
